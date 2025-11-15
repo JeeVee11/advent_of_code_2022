c7e04b388b2be3388b9649c523d0187ab079c96e
Joost van Esch <joostvanesch@pm.me>
2025-11-15 14:44:17 +0100

    Update new_day.sh

--- new_day.sh
+++ new_day.sh
@@ -3,7 +3,7 @@ set -e
 
 dir="src/$1"
 
-mkdir $dir
+mkdir -p $dir
 
 touch "$dir/in"
 
@@ -22,5 +22,10 @@ fn part2() {
   
 }
 EOF
+
+head -n -3 "src/main.rs" > tmp
+sed -i "2i mod $1;" tmp
+echo -e "fn main() {\n    $1::run();\n}" >> tmp
+mv tmp "src/main.rs"
 
 echo "Created $1"
